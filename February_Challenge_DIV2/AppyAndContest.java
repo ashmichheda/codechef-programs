5adfedeb8c341cfbbcbe83931ad86d041685a257
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-06 22:40:15 +0530

    February Challenge Div 2 Problems

---
+++ February_Challenge_DIV2/AppyAndContest.java
@@ -0,0 +1,43 @@
+//package com.prog.CodeChefCompetitionsProgs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*public*/ class AppyAndContest {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t;
+		t = Integer.parseInt(br.readLine());
+		while(t-- > 0)
+		{
+			int n, a, b, k;
+			String input[] = br.readLine().split(" ");
+			n = Integer.parseInt(input[0]);
+			a = Integer.parseInt(input[1]);	// Appy
+			b = Integer.parseInt(input[2]);	// Chef
+			k = Integer.parseInt(input[3]);	// atleast k problems to solve to win
+			int count = 0;
+			int mulVariable = a*b;
+			// logic to check no. of problem solved
+			for(int i = 1; i<=n; i++) {
+				
+				// for Appy
+				if((i % a == 0) && (i % mulVariable!= 0) && (i % b != 0))
+					count++;
+				else if(i % b == 0 && (i % mulVariable!= 0) && (i % a != 0))
+					count++;
+				if(count >= k)
+					break;
+			}
+			if(count >= k)
+				System.out.println("Win");
+			else
+				System.out.println("Lose");
+			
+			
+		}
+			
+	}
+}
