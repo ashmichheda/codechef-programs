538ce8ce35c25bccb0d76f339cf5492d9e87a8cf
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-22 22:59:43 +0530

    comment local package

--- Programming-contest-programs/ChefAndEid.java
+++ Programming-contest-programs/ChefAndEid.java
@@ -1,4 +1,4 @@
-package com.prog.CodeChefCompetitionsProgs;
+//package com.prog.CodeChefCompetitionsProgs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class ChefAndEid {
+/*public*/ class ChefAndEid {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
